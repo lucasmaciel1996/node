27f1ec5adc3e3b746d60c824e7fc8eb03c67f276
aluno <lucas333maciel@gmail.com>
2018-10-01 21:42:36 -0300

    2 Commit

---
+++ cal.js
@@ -0,0 +1,8 @@
+var module =require('./soma');
+var n1= 23;
+var n2 =32;
+
+var soma = module.soma(n1,n2);
+var mult = module.mult(n1,n2);
+
+console.log(soma,mult);
---
+++ soma.js
@@ -0,0 +1,12 @@
+function somar(n1,n2)
+{
+  return n1+n2;
+}
+
+function mult(n1,n2)
+{
+ return n1*n2;
+}
+
+module.exports.soma=somar;
+module.exports.mult=mult;
